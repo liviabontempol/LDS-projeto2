89a9302663e28daaf160cbcbc259f6ec44b12638
liviabontempol <liviabontempolivia@gmail.com>
2024-09-25 13:35:37 -0300

    estrutura code

---
+++ demo/src/main/java/com/example/demo/Models/Client.java
@@ -0,0 +1,8 @@
+package com.example.demo.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client extends User{
+    
+}
---
+++ demo/src/main/java/com/example/demo/Models/User.java
@@ -0,0 +1,29 @@
+package com.example.demo.Models;
+
+import jakarta.persistence.*;
+
+
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    String email;
+    String password;
+
+
+    //Getters e Setters
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
